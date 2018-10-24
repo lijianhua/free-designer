b3bd91cbc11264b405dd768c5f4e4a35019df36f
ranqiyang11 <ranqiyang@jiayunhui.com>
2018-10-24 17:01:59 +0800

    home页面

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        <h4>mobile home</h4>
+    <div class="home">
+        <mt-swipe class="swiper" :auto="4000">
+            <mt-swipe-item><img src="../../assets/images/banner/banner5.png" alt="banner5"></mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/images/banner/banner4.png" alt="banner4"></mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/images/banner/banner3.png" alt="banner3"></mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/images/banner/banner2.png" alt="banner2"></mt-swipe-item>
+            <mt-swipe-item><img src="../../assets/images/banner/banner1.png" alt="banner1"></mt-swipe-item>
+        </mt-swipe>
     </div>
 </template>
 
@@ -10,6 +16,13 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .home{
+        .swiper{
+            height: 308px;
+            img{
+                width: 100%;
+            }
+        }
+    }
 </style>
