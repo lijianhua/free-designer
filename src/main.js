c6e98a09c035d7fc04e2929b7d96e36b7765eed9
ranqiyang11 <ranqiyang@jiayunhui.com>
2018-10-25 16:06:46 +0800

    muse-ui main.js 引入

--- src/main.js
+++ src/main.js
@@ -3,18 +3,21 @@ import App from './App'
 import router from './router'
 import store from './store'
 import MintUI from 'mint-ui'
+import MuseUI from 'muse-ui'
 import { sync } from 'vuex-router-sync'
 
 import './assets/css/reset.css'
 import './assets/css/icon.css'
 import 'mint-ui/lib/style.css'
 import 'lib-flexible/flexible.js'
+import 'muse-ui/dist/muse-ui.css'
 
 import delay from './utils/delay'
 import hook from './router/hook'
 
 Vue.use(delay)
 Vue.use(MintUI)
+Vue.use(MuseUI)
 
 sync(store, router)
 hook(router)
