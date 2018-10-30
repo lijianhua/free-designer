1275fd12ddcd35a62005eccf8e8819d5212ed540
王建鹏 <amyas.wang@qingclass.com>
2018-10-30 11:46:19 +0800

    个人中心UI重写

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -5,162 +5,137 @@
                 <img src="http://www.free-designer.cn/M00/00/64/wKgABVt30lKAYOf6AAAz0yXWPI4179.png">
             </div>
             <div class="setting">
-                <span>个人信息设置</span>
-                <i class="fas fa-chevron-right"></i>
+                <img src="~@/assets/images/my/ico_pencil.png" alt="">
+                <span>编辑资料</span>
             </div>
             <div class="desc">
-                <div class="info">
-                    <div class="tel">137****0000</div>
-                    <div class="exp">6年从业经验</div>
+                <div>
+                    <div>2000</div>
+                    <div>可用积分</div>
                 </div>
-                <div class="integral">
-                    <div>可用积分：90890</div>
-                    <div>可提现积分：50333</div>
+                <div>
+                    <div>0</div>
+                    <div>可提现积分</div>
                 </div>
-                <div class="intro">测试一下啦</div>
+                <div>
+                    <div>0</div>
+                    <div>工作年限</div>
+                </div>
             </div>
         </div>
         <div class="list">
             <router-link class="item" :to="{name:'my-work'}">
+                <img src="../../assets/images/my/ico_work.png" alt="">
                 <p>我的作品</p>
-                <i class="fas fa-chevron-right"></i>
             </router-link>
             <router-link class="item" :to="{name:'my-certification-result'}">
+                <img src="../../assets/images/my/ico_certification.png" alt="">
                 <p>实名认证</p>
-                <i class="fas fa-chevron-right"></i>
             </router-link>
             <router-link class="item" :to="{name:'my-about'}">
-                <p>平台说明</p>
-                <i class="fas fa-chevron-right"></i>
+                <img src="../../assets/images/my/ico_about.png" alt="">
+                <p>企业说明</p>
             </router-link>
             <router-link class="item" :to="{name:'my-pay-new'}">
+                <img src="../../assets/images/my/ico_order.png" alt="">
+                <p>历史订单</p>
+            </router-link>
+            <router-link class="item" :to="{name:'my-work'}">
+                <img src="../../assets/images/my/ico_recharge.png" alt="">
                 <p>积分充值</p>
-                <i class="fas fa-chevron-right"></i>
             </router-link>
             <router-link class="item" :to="{name:'my-work'}">
+                <img src="../../assets/images/my/ico_setting.png" alt="">
                 <p>积分管理</p>
-                <i class="fas fa-chevron-right"></i>
             </router-link>
             <router-link class="item" :to="{name:'my-work'}">
-                <p>退出登录</p>
-                <i class="fas fa-chevron-right"></i>
+                <img src="../../assets/images/my/ico_msg.png" alt="">
+                <p>我的消息</p>
+            </router-link>
+            <router-link class="item" :to="{name:'my-work'}">
+                <img src="../../assets/images/my/ico_start.png" alt="">
+                <p>我的收藏</p>
             </router-link>
         </div>
     </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .my-container {
-    display: flex;
-    flex-direction: column;
+    width: 100%;
     height: 100%;
-    background: #eee;
+    background-color: #fff;
+}
+.header {
+    padding-top: 21px;
+    border-bottom: 20px solid #e5e5e5;
 
-    .header {
-        padding: 20px;
-        overflow: hidden;
-        background: url('~@/assets/images/my_background.png');
-        background-size: cover;
-        height: 440px;
+    .avatar {
+        width: 160px;
+        height: 160px;
+        margin: 0 auto 25px;
 
-        .avatar {
-            overflow: hidden;
-            border-radius: 50%;
-            background: #eee;
-            color: #fff;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+    .setting {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 65px;
+
+        img {
+            width: 23px;
+            height: 25px;
+        }
+        span {
+            padding-left: 10px;
+        }
+
+    }
+    .desc {
+        display: flex;
+        justify-content: space-between;
+        margin: 0 120px 10px;
+
+        > div {
+            font-size: 25px;
             text-align: center;
-            line-height: 120px;
-            height: 120px;
-            width: 120px;
-            margin: 0 auto;
 
-            img {
-                height: 100%;
-                width: 100%;
+            div:first-child {
+                font-weight: 500;
+                color: #535353;
             }
-        }
+            div:last-child {
+                color: #898989;
 
-        .setting {
-            font-size: 24px;
-            line-height: 48px;
-            text-align: right;
-            i {
-                color: #2196f3;
-                margin-left: 8px;
-            }
-        }
-
-        .desc {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-
-            .info,.integral {
-                display: flex;
-                margin-top: 0.4rem;
-            }
-
-            .info {
-                align-items: flex-end;
-                .tel {
-                    font-size: 40px;
-                }
-                .exp {
-                    margin-left: 40px;
-                    font-size: 28px;
-                    margin-bottom: 4px;
-                }
-            }
-
-            .integral {
-                font-size: 32px;
-                > div {
-                    margin: 0 10px;
-                }
-            }
-
-            .intro {
-                margin-top: 32px;
-                font-size: 24px;
-                line-height: 48px;
             }
         }
 
     }
+}
+.list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 39px 118px 0;
 
-    .list {
-        flex: 1;
-        background: #fff;
-        padding: 20px;
-        position: relative;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        display: inline-block;
-        width: 100%;
-        overflow: auto;
-        overflow-scrolling: touch;
-        margin-top: 40px;
-        font-weight: 300;
-        color: #aaa;
-        box-sizing: border-box;
-
-        .item {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            line-height: 80px;
-            font-size: 32px;
-            color: #999;
-            border-bottom: 1px solid #eee;
+    >a {
+        text-align: center;
+        margin-right: 109px;
+        img {
+            width: 40px;
         }
+        p {
+            padding-top: 24px;
+        }
+    }
+
+    >a:nth-child(3n+0){
+        margin-right: 0;
+    }
+    >a:nth-child(n+4){
+        margin-top: 50px;
     }
 }
 </style>
